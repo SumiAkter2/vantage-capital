875da8c2b8dc6d072c5c4560f39d7b06201e0bfc
SumiAkter2 <akteralisha36@gmail.com>
2023-10-01 09:20:07 +0600

    added fund form

--- src/Component/Fund/Fund.jsx
+++ src/Component/Fund/Fund.jsx
@@ -1,57 +1,132 @@
 // import { BsFillArrowRightCircleFill } from "react-icons/bs";
 import { BiBullseye } from "react-icons/bi";
-import './Fund.css'
+import "./Fund.css";
 const Fund = () => {
   return (
-    <div className="hero min-h-screen ">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="text"
-                placeholder="email"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="text"
-                placeholder="password"
-                className="input input-bordered"
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
-            </div>
+    <div>
+      <div className="h-56 bg-[#202e3d]">
+        <h1 className="text-5xl text-center pt-32 font-bold pb-6">
+          Liquidity Fund Investors
+        </h1>
+      </div>
+      <div
+        className="card p-12 w-[300px] md:w-[700px] shadow-2xl  mt-6 mx-auto"
+        style={{
+          backgroundImage: "linear-gradient(#202e3d, #3a5371, #202e3d)",
+        }}
+      >
+        <div>
+          <p className="bg-slate-600 p-6 rounded-md ">
+            <span className="font-semibold ">
+              Min : 5.000 $ / Max : 500.000 $ <br /> Lock in Period : 90 Days
+              <br /> Profit Rate : %4 - %7 (Monthly)
+            </span>
+          </p>
+          <p className="text-xl py-4">
+            If you're interested in our Liquidity Fund, please provide us with
+            some initial information:
+          </p>
+          <div className="form-control ">
+            <label className="label">
+              <span className="label-text">Your name:</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Name"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control ">
+            <label className="label">
+              <span className="label-text">Investment amount:</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Amount"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control ">
+            <label className="label">
+              <span className="label-text">Preferred contact email:</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Email"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">
+                Any additional questions or comments?
+              </span>
+            </label>
+            <input
+              type="text"
+              placeholder="Text here"
+              className="textarea  textarea-bordered"
+            />
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Submit</button>
           </div>
         </div>
-        <div
-          className="hero min-h-screen md:min-w-full my-24 info-container relative"
-         
-        >
-          <div className="hero-overlay bg-black bg-opacity-60 w-2/3 absolute left-0"></div>
-          <div className="hero-content  text-neutral-content">
-            <div className="max-w-md">
-              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-              <p className="mb-5">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-              <button className="btn btn-primary">Get Started</button>
-            </div>
+      </div>
+
+      <div className="hero min-h-screen my-6 info-container relative">
+        <div className="hero-overlay bg-black bg-opacity-80 w-4/5 absolute left-0"></div>
+        <div className="hero-content  text-white ">
+          <div className="w-4/5 absolute left-8 px-12 ">
+            <h1 className="my-5 text-3xl font-bold uppercase flex items-center">
+              <BiBullseye size="25px" className="mr-2" /> What is The Liquidity
+              Fund ?
+            </h1>
+            <p className="mb-2">
+              A liquidity fund is a type of investment fund that focuses on
+              safety and easy access to cash. It primarily invests in
+              short-term, low-risk securities like Treasury bills, commercial
+              paper, and certificates of deposit. The goal is to preserve the
+              invested money while providing quick access to it. Liquidity funds
+              offer stable share prices, are highly liquid, and are suitable for
+              holding cash reserves or short-term savings, although they offer
+              relatively low returns compared to riskier investments. They are
+              subject to regulatory oversight to protect investors.
+            </p>
+            <p>
+              Discover our liquidity Fund, a unique investment opportunity
+              designed for low-risk, high-return seekers.
+            </p>
+            <h1 className="my-4 text-3xl font-bold uppercase flex items-center">
+              <BiBullseye size="25px" className="mr-2" /> How It Works ?
+            </h1>
+            <p className="mb-5">
+              Zero Risk Investment: Your capital remains secure, as it doesn’t
+              directly engage in trading activities.
+            </p>
+            <h1 className="my-4 text-3xl font-bold uppercase flex items-center">
+              <BiBullseye size="25px" className="mr-2" /> Benefits
+            </h1>
+            <p className="mb-5">
+              “Secure Your Future: Invest in our Illiquidity Fund and enjoy
+              expected returns of 7% to 10% per month.”
+            </p>
+            <h1 className="my-4 text-3xl font-bold uppercase flex items-center">
+              <BiBullseye size="25px" className="mr-2" />
+              Why Choose Us ?
+            </h1>
+            <p className="mb-5">
+              Trust in Experience: With our years of expertise, we provide
+              liquidity to traders for profitable volume trading.
+            </p>
+            <h1 className="my-4 text-3xl font-bold uppercase flex items-center">
+              <BiBullseye size="25px" className="mr-2" />
+              Call to Action (CTA)
+            </h1>
+            <p className="mb-5">
+              Get Started Today: Invest in the liquidity Fund and watch your
+              wealth grow.
+            </p>
           </div>
         </div>
       </div>
