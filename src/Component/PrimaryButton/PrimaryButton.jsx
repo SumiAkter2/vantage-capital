c20d810a99ecd860bf1a34aa1303ef336db6bfbf
SumiAkter2 <akteralisha36@gmail.com>
2024-03-19 16:23:29 +0600

    done

--- src/Component/PrimaryButton/PrimaryButton.jsx
+++ src/Component/PrimaryButton/PrimaryButton.jsx
@@ -1,7 +1,6 @@
 import "./Button.css";
 
 const PrimaryButton = ({ title }) => {
-  // const link = "  https://one.exnesstrack.net/a/d2t409aa77 ";
   return (
     <div>
       <a href="https://one.exnesstrack.net/a/d2t409aa77">
